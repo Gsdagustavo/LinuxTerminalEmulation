377c3ea8ea53559835e95d6545c642bd05b36651
Gustavo Daniel <gugadanielalvez@gmail.com>
2024-07-06 10:45:01 -0300

    fixed clear and echo command; started exit command

--- src/application/Main.java
+++ src/application/Main.java
@@ -1,7 +1,5 @@
 package application;
 
-import java.util.ArrayList;
-import java.util.List;
 import java.util.Scanner;
 
 public class Main {
@@ -18,7 +16,8 @@ public class Main {
             showInputLine();
             String input = sc.nextLine();
 
-            validateCommand(input);
+            int returnCode = validateCommand(input);
+//            System.out.println(returnCode);
         }
 
     }
@@ -27,39 +26,36 @@ public class Main {
         System.out.print("user@user $ ");
     }
 
-    public static void validateCommand(String userInput) {
+    public static int validateCommand(String userInput) {
         String[] arguments = userInput.split(" ");
-        int argumentsSize = arguments.length;
 
         String firsArgument = arguments[0];
 
+        // Execute commands based on the command and arguments received
         for (int i = 0; i < commands.length; i++) {
 
+            // Echo command
             if (firsArgument.compareTo("echo") == 0) {
+                echoCommand(arguments);
+                return 0;
 
-                if (arguments.length == 1) {
-                    return;
-                }
-
-                for (int j = 0; i < arguments.length; i++) {
-
-                    if (i == 0) { continue; }
-                    System.out.print(arguments[i]);
-
-                    System.out.print(" ");
-                }
-
-//                wait(100);
-                System.out.println();
-                return;
+            // Clear command
             } else if (firsArgument.compareTo("clear") == 0) {
                 clearCommand();
-                return;
+                return 0;
+            } else if (firsArgument.compareTo("exit") == 0) {
+
             }
+        }
+
+        if (userInput.isBlank()) {
+            return -1;
         }
 
         System.err.println(userInput + " command not recognized.");
         wait(100);
+
+        return -1;
     }
 
     public static void wait(int millis) {
@@ -68,15 +64,35 @@ public class Main {
         } catch (InterruptedException ignored) { }
     }
 
-    public static void echoCommand(String arg) {
-        System.out.println(arg);
-    }
-
     public static void clearCommand() {
         for (int i = 0; i < 50; i++) {
             System.out.println();
         }
     }
 
-//    public static void q
+    public static int echoCommand(String[] arguments) {
+        // Will return if there are no arguments
+        if (arguments.length == 1) {
+            return 1;
+        }
+
+        // Print the arguments in the console
+        for (int i = 0; i < arguments.length; i++) {
+
+            if (i == 0) { continue; }
+            System.out.print(arguments[i]);
+
+            if (!((i + 1) == arguments.length)) {
+                System.out.print(" ");
+            }
+        }
+
+        System.out.println();
+
+        return 0;
+    }
+
+    public static int exitCommand(String[] args) {
+        
+    }
 }
